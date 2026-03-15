184e55d8d12d3852db41a9b317d3c3132fa69c55
Daniel Nesterov <KOfTD@yandex.ru>
2026-03-15 17:17:47 +0300

    refactor: get rid of the redundant class wrapper

--- tests/convolution_test.py
+++ tests/convolution_test.py
@@ -9,87 +9,85 @@ import torch.nn as tnn
 from utils import TensorShape, compute_conv, compute_shape
 
 
-class TestComputeConv:
-    def test_conv(self):
-        result = compute_conv(227, 2, 11, 4)
-        assert result == 56
+def test_compute_conv():
+    result = compute_conv(227, 2, 11, 4)
+    assert result == 56
 
 
-class TestComputShape:
-    def test_conv(self):
-        shape = TensorShape(32, 32, 3)
-        padding = (2, 2)
-        kernel_size = (5, 5)
-        stride = (3, 3)
-        conv = tnn.Conv2d(
-            3, 64, padding=padding, kernel_size=kernel_size, stride=stride
-        )
-        result = compute_shape(conv, shape)
+def test_conv():
+    shape = TensorShape(32, 32, 3)
+    padding = (2, 2)
+    kernel_size = (5, 5)
+    stride = (3, 3)
+    conv = tnn.Conv2d(3, 64, padding=padding, kernel_size=kernel_size, stride=stride)
+    result = compute_shape(conv, shape)
 
-        checker_height = compute_conv(
-            shape.height, padding[0], kernel_size[0], stride[0]
-        )
-        checker_width = compute_conv(shape.width, padding[1], kernel_size[1], stride[1])
-        checker = TensorShape(checker_height, checker_width, 64)
+    checker_height = compute_conv(shape.height, padding[0], kernel_size[0], stride[0])
+    checker_width = compute_conv(shape.width, padding[1], kernel_size[1], stride[1])
+    checker = TensorShape(checker_height, checker_width, 64)
 
-        assert result == checker
+    assert result == checker
 
-    def test_relu(self):
-        activation = tnn.ReLU()
-        shape = TensorShape(32, 32, 3)
 
-        result = compute_shape(activation, shape)
+def test_relu():
+    activation = tnn.ReLU()
+    shape = TensorShape(32, 32, 3)
 
-        assert result == shape
+    result = compute_shape(activation, shape)
 
-    def test_dropout(self):
-        activation = tnn.Dropout(0.5)
-        shape = TensorShape(32, 32, 3)
+    assert result == shape
 
-        result = compute_shape(activation, shape)
 
-        assert result == shape
+def test_dropout():
+    activation = tnn.Dropout(0.5)
+    shape = TensorShape(32, 32, 3)
 
-    def test_batch_norm(self):
-        activation = tnn.BatchNorm2d(3024)
-        shape = TensorShape(32, 32, 3)
+    result = compute_shape(activation, shape)
 
-        result = compute_shape(activation, shape)
+    assert result == shape
 
-        assert result == shape
 
-    def test_max_pool(self):
-        shape = TensorShape(32, 32, 3)
-        padding = (2, 2)
-        kernel_size = (5, 5)
-        stride = (3, 3)
-        pool = tnn.MaxPool2d(kernel_size[0], stride[0], padding[0])
-        result = compute_shape(pool, shape)
+def test_batch_norm():
+    activation = tnn.BatchNorm2d(3024)
+    shape = TensorShape(32, 32, 3)
 
-        checker_height = compute_conv(
-            shape.height, padding[0], kernel_size[0], stride[0]
-        )
-        checker_width = compute_conv(shape.width, padding[1], kernel_size[1], stride[1])
-        checker = TensorShape(checker_height, checker_width, 3)
+    result = compute_shape(activation, shape)
 
-        assert result == checker
+    assert result == shape
 
-    def test_adaptive_max_pool(self):
-        shape = TensorShape(32, 32, 3)
-        out_size = (3, 3)
-        pool = tnn.AdaptiveMaxPool2d(out_size)
-        result = compute_shape(pool, shape)
 
-        checker = TensorShape(out_size[0], out_size[1], 3)
+def test_max_pool():
+    shape = TensorShape(32, 32, 3)
+    padding = (2, 2)
+    kernel_size = (5, 5)
+    stride = (3, 3)
+    pool = tnn.MaxPool2d(kernel_size[0], stride[0], padding[0])
+    result = compute_shape(pool, shape)
 
-        assert result == checker
+    checker_height = compute_conv(shape.height, padding[0], kernel_size[0], stride[0])
+    checker_width = compute_conv(shape.width, padding[1], kernel_size[1], stride[1])
+    checker = TensorShape(checker_height, checker_width, 3)
 
-    def test_adaptive_avg_pool(self):
-        shape = TensorShape(32, 32, 3)
-        out_size = (3, 3)
-        pool = tnn.AdaptiveAvgPool2d(out_size)
-        result = compute_shape(pool, shape)
+    assert result == checker
 
-        checker = TensorShape(out_size[0], out_size[1], 3)
 
-        assert result == checker
+def test_adaptive_max_pool():
+    shape = TensorShape(32, 32, 3)
+    out_size = (3, 3)
+    pool = tnn.AdaptiveMaxPool2d(out_size)
+    result = compute_shape(pool, shape)
+
+    checker = TensorShape(out_size[0], out_size[1], 3)
+
+    assert result == checker
+
+
+def test_adaptive_avg_pool():
+    shape = TensorShape(32, 32, 3)
+    out_size = (3, 3)
+    pool = tnn.AdaptiveAvgPool2d(out_size)
+    result = compute_shape(pool, shape)
+
+    checker = TensorShape(out_size[0], out_size[1], 3)
+
+    assert result == checker
