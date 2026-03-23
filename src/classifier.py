e55ced600d260d24c72d634aa9b932bf2e2852e9
Daniel Nesterov <KOfTD@yandex.ru>
2026-03-23 08:56:39 +0300

    refactor: simplify classifier

--- src/classifier.py
+++ src/classifier.py
@@ -1,72 +1,77 @@
-from collections.abc import Iterable
-from functools import singledispatch
-from typing import Self
+from collections.abc import Iterable, Sequence
+from typing import Self, cast, overload
 
 import torch.nn as tnn
 
 from utils import TensorShape
 
+__all__ = ["Classifier"]
+
+
+def _reject_conv2d(modules: Iterable[tnn.Module]) -> None:
+    if any(isinstance(m, tnn.Conv2d) for m in modules):
+        raise ValueError("Convolution layers cannot be in classifier")
+
 
 class Classifier:
     def __init__(
-        self, modules: list[tnn.Module], input_shape: TensorShape | int
+        self, modules: Sequence[tnn.Module], input_shape: TensorShape | int
     ) -> None:
-        if any(isinstance(module, tnn.Conv2d) for module in modules):
-            raise ValueError("Convolution layers can not be in classifier")
+        _reject_conv2d(modules)
 
         in_features = (
             input_shape.in_features()
             if isinstance(input_shape, TensorShape)
             else input_shape
         )
-        self._modules: list[tnn.Module] = modules
+
+        self._layers: list[tnn.Module] = list(modules)
         previous_features: int = in_features
 
         for i, module in enumerate(modules):
             if isinstance(module, tnn.Linear):
-                self._modules[i] = tnn.Linear(previous_features, module.out_features)
+                self._layers[i] = tnn.Linear(previous_features, module.out_features)
                 previous_features = module.out_features
 
         self._out_features = previous_features
 
     def __repr__(self) -> str:
-        return str(self._modules)
+        return str(self._layers)
 
     def append(self, module: tnn.Module) -> None:
         if isinstance(module, tnn.Conv2d):
-            raise ValueError("Convolution layers can not be in classifier")
+            raise ValueError("Convolution layers cannot be in classifier")
 
         if isinstance(module, tnn.Linear):
             tail = tnn.Linear(self._out_features, module.out_features)
         else:
             tail = module
 
-        self._modules.append(tail)
+        self._layers.append(tail)
         self._out_features = (
             tail.out_features if isinstance(tail, tnn.Linear) else self._out_features
         )
 
-    @singledispatch
-    def extend(self, arg: Iterable[tnn.Module] | Self) -> None:
-        if isinstance(arg, self.__class__):
-            for module in arg._modules:
+    @overload
+    def extend(self, modules: Self) -> None: ...
+
+    @overload
+    def extend(self, modules: Iterable[tnn.Module]) -> None: ...
+
+    def extend(self, modules: Iterable[tnn.Module] | Self) -> None:
+        if isinstance(modules, self.__class__):
+            for module in modules._layers:
                 self.append(module)
         else:
-            raise NotImplementedError(
-                f"Unsupported argument type: {type(arg).__name__}"
-            )
-
-    @extend.register(Iterable)
-    def _(self, modules: Iterable[tnn.Module]) -> None:
-        if any(isinstance(module, tnn.Conv2d) for module in modules):
-            raise ValueError("Convolution layers can not be in classifier")
-
-        for module in modules:
-            self.append(module)
+            layers = cast(Iterable[tnn.Module], modules)
+            layers = list(layers)
+            _reject_conv2d(layers)
+            for module in layers:
+                self.append(module)
 
     @property
     def out_features(self) -> int:
         return self._out_features
 
     def sequential(self) -> tnn.Sequential:
-        return tnn.Sequential(*self._modules)
+        return tnn.Sequential(*self._layers)
